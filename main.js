41bbbd2a291ae75fb85c09305a1562a365bdafc9
Gage Bachik <gbachik@gmail.com>
2014-07-09 21:02:28 -0600

    updated

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+function init(){
+	$('.menu-bar').on('click', '.coding', function(event){
+		event.preventDefault();
+		$('.ajax-swap').load('coding.html');
+	});
+	$('.menu-bar').on('click', '.products', function(event){
+		event.preventDefault();
+		$('.ajax-swap').load('products.html');
+	});
+	$('.menu-bar').on('click', '.services', function(event){
+		event.preventDefault();
+		$('.ajax-swap').load('services.html');
+	});
+}
+
 $(document).on('ready', function() {
-  
+	init();
 });

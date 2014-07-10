d1a587286d6c3d8cfda6673d4b6b2760d40d3ac0
Gage Bachik <gbachik@gmail.com>
2014-07-09 21:52:17 -0600

    updated

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ function init(){
 	$('.menu-bar').on('click', '.coding', function(event){
 		event.preventDefault();
 		$('.ajax-swap').load('coding.html');
+		$('.products').closest('li').removeClass('active');
+		$('.services').closest('li').removeClass('active');
+		$(this).closest('li').addClass('active');
 	});
 	$('.menu-bar').on('click', '.products', function(event){
 		event.preventDefault();
 		$('.ajax-swap').load('products.html');
+		$('.services').closest('li').removeClass('active');
+		$('.coding').closest('li').removeClass('active');
+		$(this).closest('li').addClass('active');
 	});
 	$('.menu-bar').on('click', '.services', function(event){
 		event.preventDefault();
 		$('.ajax-swap').load('services.html');
+		$('.coding').closest('li').removeClass('active');
+		$('.products').closest('li').removeClass('active');
+		$(this).closest('li').addClass('active');
 	});
 }
 
